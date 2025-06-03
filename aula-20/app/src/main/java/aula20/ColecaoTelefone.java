5fc0e8eae6aace70271d6bd3e2a14b0cb164618d
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-06-02 22:30:23 -0300

    agenda telefonica

---
+++ aula-20/app/src/main/java/aula20/ColecaoTelefone.java
@@ -0,0 +1,21 @@
+package main.java.aula20;
+
+
+public class ColecaoTelefone {
+
+    private Map<String, String> dados;
+
+    
+
+    public ColecaoTelefone() {
+        this.dados = new HashMap();
+    }
+
+
+
+    public add(String rotulo, ){
+
+    }
+
+
+}
